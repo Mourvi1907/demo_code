ef021a28ff5ca928054586e585731a0c01ee599b
i-amitb <amit.bhongade1@gmail.com>
2023-07-07 00:46:32 +0530

    updating cde1

---
+++ ABC/Testing.py
@@ -0,0 +1,21 @@
+#a = {1: {'course': 'python', 'fees': 15000},
+    # 2: {'course': 'javascript', 'fees': 100000}}
+#print(a)
+
+#print(a[1])
+#print(a[2])
+
+#print(a[1]['course'])
+#print(a[2]['fees'])
+
+#a[1]['course'] = ['machine learning']
+#print(a)
+#-------------------------------------------------
+
+def add():
+     a=10
+     b=20
+     c=a+b
+     print(c)
+
+add()
